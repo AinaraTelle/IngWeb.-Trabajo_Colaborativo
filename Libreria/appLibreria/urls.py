f18d998557b8aee06a195e6fe5f0542a6c87a1af
Helene Madurga <helene.madurga@opendeusto.es>
2025-11-14 19:03:34 +0100

    Vistas pt1

--- Libreria/appLibreria/urls.py
+++ Libreria/appLibreria/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('genero/<str:genero>/', views.Libro, name='genero'),
+    path('fecha/<str:fecha_libro>/', views.Libro, name='antiguedad'), #no portada 
+    path('precio/<int:precio_libro/', views.Libro, name='precio'),
 ]
--- Libreria/appLibreria/views.py
+++ Libreria/appLibreria/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def genero(request, genero_libro):
+    return HttpResponse("Consultando genero del libro %s." %genero_libro)
+
+def portadaYAntiguedad(request, fecha_libro): #devuelve la portada tambien pero no he encontrado el atributo en modelos por lo que aun no añado
+    return HttpResponse("Portada y antiguedad del libro %s" % fecha_libro) #no puedo devolver portada si no esta
+
+def precio(request, precio_libro):
+    return HttpResponse("Precio de libro %f" %precio_libro)
